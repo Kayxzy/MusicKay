f929a7e270aa61a097eb0117fb77577db604ee7b
Kayxzy <106707683+Kayxzy@users.noreply.github.com>
2022-06-16 03:35:26 +0700

    Update Play.py

--- Yukki/Plugins/Play.py
+++ Yukki/Plugins/Play.py
@@ -166,8 +166,7 @@ async def play(_, message: Message):
                 message.from_user.first_name, message.from_user.id, "abcd"
             )
             await message.reply_text(
-                    (
-                    "**Usage:** /play [Music Name or Youtube Link or Reply to Audio]\n\nIf you want to play Playlists! Select the one from Below."
+            message=("**Usage:** /play [Music Name or Youtube Link or Reply to Audio]\n\nIf you want to play Playlists! Select the one from Below."
                 ),
                 reply_markup=InlineKeyboardMarkup(buttons),
             )
